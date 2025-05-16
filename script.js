75c7910cadebdcc06f4d98bc960053b7a2ea1efc
Arthur Paiva <paivaarthur16@gmail.com>
2025-05-16 17:59:20 -0300

    restaurado

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-// Este script JavaScript pode ser expandido para adicionar funcionalidades interativas.
-console.log("Script carregado!");
+$(document).ready(function(){
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+});
