a0519bffba63f2c4145c0a1e8cd4c9db4027aaf1
Arthur Paiva <paivaarthur16@gmail.com>
2025-06-28 21:47:19 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('a[href^="#"]').on('click', function(event) {
         var target = $(this.getAttribute('href'));
-        if( target.length ) {
+        if (target.length) {
             event.preventDefault();
             $('html, body').stop().animate({
                 scrollTop: target.offset().top
             }, 1000);
         }
+    });
+
+    $('.faq-pergunta').on('click', function() {
+        const item = $(this).closest('.faq-item');
+        item.toggleClass('active');
+        $('.faq-item').not(item).removeClass('active');
     });
 });
